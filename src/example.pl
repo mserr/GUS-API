d03cb7dfc6f1ca6524ac7417648142585582d73e
Michał Górski <michalmarcingorski@gmail.com>
2015-07-21 21:11:20 +0200

    get_captcha function used in example script

--- src/example.pl
+++ src/example.pl
@@ -2,7 +2,12 @@
 
 use lib '../lib';
 use Modern::Perl;
-use GUS::API qw(login);
+use GUS::API qw(login get_captcha);
 
 
-say login('gopo49n9gbj303og6ny7');
+my $session_id = login('gopo49n9gbj303og6ny7');
+say 'Your session id: '.$session_id;
+
+my $captcha = get_captcha($session_id); 
+say 'Read your numbers:';
+say $captcha;
