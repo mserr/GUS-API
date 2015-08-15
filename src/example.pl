af347c41c80ff9aa18385f484d41d21185177f3b
Michał Górski <michalmarcingorski@gmail.com>
2015-08-15 23:49:36 +0200

    Use of search_nip in example script

--- src/example.pl
+++ src/example.pl
@@ -2,7 +2,7 @@
 
 use lib '../lib';
 use Modern::Perl;
-use GUS::API qw(login get_captcha captcha2jpeg check_captcha);
+use GUS::API qw(login get_captcha captcha2jpeg check_captcha search_nip);
 
 
 my $session_id = login('gopo49n9gbj303og6ny7');
@@ -12,10 +12,15 @@ my $captcha = get_captcha($session_id);
 say 'Read your numbers:';
 say $captcha;
 
+say '---------------------------------------------------------';
 say 'You will find decoded picture at /tmp/decoded_captcha.jpg';
 captcha2jpeg($captcha, '/tmp/decoded_captcha.jpg');
 
 if ( check_captcha( $session_id, 'aaaaa' ) ){
   say 'Captcha was succesfully verified!';
 };
+
+my $nip = '9570949817';
+my $data = search_nip($session_id, $nip);
+say 'Contractors name: '.$data->{dane}->{"Nazwa"}.' for NIP:'.$nip;
 
